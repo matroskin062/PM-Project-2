811a427c1ca1a4a1412c4e3e2de0e287575559b0
Dima Matroskin <dima.matroskin062@gmail.com>
2021-02-23 13:14:16 +0200

    ColumnUI created

---
+++ src/js/ColumnUI.js
@@ -0,0 +1,29 @@
+class ColumnUI {
+  constructor(column, cards) {
+    this.columnContainer = document.querySelector('.column-list');
+    this.status = column.value;
+    this.title = column.title;
+    this.cards = cards;
+  }
+
+  createColumn() {
+    const column = document.createElement('li');
+    column.classList.add('column');
+
+    const title = document.createElement('p');
+    title.classList.add('title');
+    title.textContent = this.title;
+
+    const cardList = document.createElement('ul');
+    cardList.classList.add('card-list');
+
+    column.append(title);
+    /* TODO: Form */
+    column.append(cardList);
+
+    this.columnContainer.append(column);
+  }
+  // TODO: Cards rendering
+}
+
+export default ColumnUI;
