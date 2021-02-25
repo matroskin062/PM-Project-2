8da3fd5996536b473230fcca8d05c443d44861d2
Artem Martyniuk <muartem99@gmail.com>
2021-02-25 21:44:38 +0200

    chek User token

--- src/js/App.js
+++ src/js/App.js
@@ -1,12 +1,11 @@
 import BoardUI from './BoardUI';
 import AuthUI from './AuthUI';
-import UserUI from './UserUI'
+import UserUI from './UserUI';
 
 export default class App {
   static init() {
-    new BoardUI().render()
+    new BoardUI().render();
     new AuthUI().init();
     new UserUI().init();
   }
-
 }
--- src/js/UserUI.js
+++ src/js/UserUI.js
@@ -1,24 +1,24 @@
+import User from './User';
+
 export default class UserUI {
   constructor() {
     this.AuthHead = document.querySelector('.auth');
     this.UserHead = document.querySelector('.user-info');
     this.userName = document.querySelector('#username-in-head');
-
   }
 
   init() {
-    const {AuthHead, UserHead, userName} = this;
-    const user = localStorage.getItem('username')
-    if(user){
+    const { AuthHead, UserHead, userName } = this;
+    const user = User.username;
+    if (User.token) {
       userName.innerText = user;
       UserHead.classList.add('active');
       AuthHead.classList.remove('active');
-      return true
+      return true;
     }
-    userName.innerText = "";
+    userName.innerText = '';
     AuthHead.classList.add('active');
     UserHead.classList.remove('active');
-    return false
+    return false;
   }
-
 }
