aca98abdb8481899458e640552ba763c95e32de0
Sasha Hulitskyi <alexgulitskyy3@gmail.com>
2021-02-25 21:03:05 +0200

    added emitter subscribe for loggedIn

--- src/js/BoardUI.js
+++ src/js/BoardUI.js
@@ -1,7 +1,16 @@
 import ColumnUI from './ColumnUI';
 import CardAPI from './CardAPI';
+import User from './User';
+import emitter from './EventEmitter';
 
 class BoardUI {
+  constructor() {
+    this.columnContainer = document.querySelector('.column-list');
+    this.init = this.init.bind(this);
+
+    emitter.subscribe('loggedIn', this.init);
+  }
+
   async render() {
     this.statuses = await CardAPI.getCardStatuses();
     this.cards = await CardAPI.getCards();
@@ -11,6 +20,14 @@ class BoardUI {
       );
       new ColumnUI(status, colCards).createColumn();
     });
+  }
+
+  init() {
+    if (User.token) {
+      this.render();
+    } else {
+      this.columnContainer.innerHTML = '';
+    }
   }
 }
 
